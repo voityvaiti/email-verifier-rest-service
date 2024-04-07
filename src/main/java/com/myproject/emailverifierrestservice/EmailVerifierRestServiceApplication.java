003c317cca816d42cbcf806680b9aeaeb163b68c
voityvaiti <flashlight@i.ua>
2024-04-07 17:04:05 +0300

    Init

---
+++ src/main/java/com/myproject/emailverifierrestservice/EmailVerifierRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.emailverifierrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerifierRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVerifierRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/emailverifierrestservice/EmailVerifierRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.emailverifierrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerifierRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
