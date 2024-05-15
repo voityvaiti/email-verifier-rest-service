27e9cb23575c52a44a0c30ca59c8815f85ae70e2
voityvaiti <flashlight@i.ua>
2024-05-15 15:33:41 +0300

    MainController annotated with @Tag and @Operation

--- src/main/java/com/myproject/emailverifierrestservice/controller/MainController.java
+++ src/main/java/com/myproject/emailverifierrestservice/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.myproject.emailverifierrestservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @PreAuthorize("permitAll()")
+@Tag(name = "Main")
 public class MainController {
 
+    @Operation(summary = "Ping", description = "Endpoint for testing server availability")
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok("pong");
